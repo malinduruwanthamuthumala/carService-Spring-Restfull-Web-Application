7bbba99ee69b7af0cd34cfa709f449138d0b5565
Malindu Ruwantha <malinduruwantha@gmail.com>
2021-06-16 07:54:30 -0700

    Internationalization

--- src/main/java/com/spring/restfull/carservice/carservice/HelloWorldController.java
+++ src/main/java/com/spring/restfull/carservice/carservice/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.spring.restfull.carservice.carservice;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
+	
 	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	public String helloWorld() {
 		return "Hello world";
@@ -28,4 +37,11 @@ public class HelloWorldController {
 	public HelloWorld helloWorldBean(@PathVariable String name) {
 		return new HelloWorld("Good Morning" + " "+ name);
 	}
+	
+	@GetMapping("/hello-internationalization")
+	public String helloInternationalized(@RequestHeader(name="Accept-Language") Locale locale) {
+		return messageSource.getMessage("good.morning.message", null,locale);
+	}
+	
+	
 }
